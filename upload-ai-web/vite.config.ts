31c287ce2b3b7a81c417297e69e582d23c545a20
Paulo Santos <paulorobcss@gmail.com>
2023-09-15 00:54:54 -0300

    final do último dia

--- upload-ai-web/vite.config.ts
+++ upload-ai-web/vite.config.ts
@@ -4,6 +4,12 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: [
+      '@ffmpeg/ffmpeg',
+      '@ffmpeg/util'
+    ]
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
